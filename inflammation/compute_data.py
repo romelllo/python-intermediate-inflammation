075ee5dd4b261ef947a1a1745df3bec96ce20b24
rnovikov <roman.novikov@cruk.manchester.ac.uk>
2026-04-16 11:22:13 +0100

    Add new module for analyzing data

---
+++ inflammation/compute_data.py
@@ -0,0 +1,45 @@
+import os
+import glob
+
+import numpy as np
+
+from inflammation import models
+
+
+class CSVDataSource:
+    """
+    Loads all the inflammation CSV files within a specified directory.
+    """
+
+    def __init__(self, dir_path):
+        self.dir_path = dir_path
+
+    def load_inflammation_data(self):
+        data_file_paths = glob.glob(os.path.join(self.dir_path, "inflammation*.csv"))
+        if len(data_file_paths) == 0:
+            raise ValueError(f"No inflammation CSV files found in path {self.dir_path}")
+        data = map(models.load_csv, data_file_paths)
+        return list(data)
+
+
+def compute_standard_deviation_by_day(data):
+    means_by_day = map(models.daily_mean, data)
+    means_by_day_matrix = np.stack(list(means_by_day))
+
+    daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
+    return daily_standard_deviation
+
+
+def analyse_data(data_source):
+    """Calculates the standard deviation by day between datasets.
+    Gets all the inflammation data from CSV files within a directory, works out the mean
+    inflammation value for each day across all datasets, then visualises the
+    standard deviation of these means on a graph."""
+    data = data_source.load_inflammation_data()
+    daily_standard_deviation = compute_standard_deviation_by_day(data)
+
+    graph_data = {
+        "standard deviation by day": daily_standard_deviation,
+    }
+    # views.visualize(graph_data)
+    return daily_standard_deviation
