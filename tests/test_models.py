c2c29d0af25a1bb9c38b995f837243a4d50ddfdc
rnovikov <roman.novikov@cruk.manchester.ac.uk>
2026-04-16 11:22:37 +0100

    Add tests for analysing data funv

--- tests/test_models.py
+++ tests/test_models.py
@@ -5,8 +5,20 @@ import numpy.testing as npt
 import pytest
 import tempfile
 import os
+import math
 
-from inflammation.models import daily_mean, daily_max, daily_min, load_csv, patient_normalise
+from inflammation.models import (
+    daily_mean,
+    daily_max,
+    daily_min,
+    load_csv,
+    patient_normalise,
+)
+from inflammation.compute_data import (
+    compute_standard_deviation_by_day,
+    analyse_data,
+    CSVDataSource,
+)
 
 
 @pytest.mark.parametrize(
@@ -54,14 +66,86 @@ def test_daily_min(test_input, expected_result):
 @pytest.mark.parametrize(
     "test, expected",
     [
-        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]], [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]])
-    ])
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+        )
+    ],
+)
 def test_patient_normalise(test, expected):
     """Test normalisation works for arrays of one and positive integers.
-       Test with a relative and absolute tolerance of 0.01."""
+    Test with a relative and absolute tolerance of 0.01."""
 
     result = patient_normalise(np.array(test))
     npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+
+
+def test_analyse_data():
+    path = os.path.join(os.getcwd(), "data")
+    data_source = CSVDataSource(path)
+    result = analyse_data(data_source)
+    expected_output = [
+        0.0,
+        0.22510286,
+        0.18157299,
+        0.1264423,
+        0.9495481,
+        0.27118211,
+        0.25104719,
+        0.22330897,
+        0.89680503,
+        0.21573875,
+        1.24235548,
+        0.63042094,
+        1.57511696,
+        2.18850242,
+        0.3729574,
+        0.69395538,
+        2.52365162,
+        0.3179312,
+        1.22850657,
+        1.63149639,
+        2.45861227,
+        1.55556052,
+        2.8214853,
+        0.92117578,
+        0.76176979,
+        2.18346188,
+        0.55368435,
+        1.78441632,
+        0.26549221,
+        1.43938417,
+        0.78959769,
+        0.64913879,
+        1.16078544,
+        0.42417995,
+        0.36019114,
+        0.80801707,
+        0.50323031,
+        0.47574665,
+        0.45197398,
+        0.22070227,
+    ]
+    npt.assert_array_almost_equal(result, expected_output)
+
+
+@pytest.mark.parametrize(
+    "data,expected_output",
+    [
+        ([[[0, 1, 0], [0, 2, 0]]], [0, 0, 0]),
+        ([[[0, 2, 0]], [[0, 1, 0]]], [0, math.sqrt(0.25), 0]),
+        ([[[0, 1, 0], [0, 2, 0]], [[0, 1, 0], [0, 2, 0]]], [0, 0, 0]),
+    ],
+    ids=[
+        "Two patients in same file",
+        "Two patients in different files",
+        "Two identical patients in two different files",
+    ],
+)
+def test_compute_standard_deviation_by_day(data, expected_output):
+
+    result = compute_standard_deviation_by_day(data)
+    npt.assert_array_almost_equal(result, expected_output)
 
 
 def test_load_csv():
